c56a0a1c45c4f6a4fb9a879688aa0630edb3688f
Patrick Pun <patrickwypun04@gmail.com>
2024-07-12 20:22:47 +0100

    add pull flag and remove tag jobs

--- build-local.sh
+++ build-local.sh
@@ -5,4 +5,4 @@ PODMAN=${PODMAN:=$(which docker)}
 distro=${@:-"el8 el9 deb12"}
 slurm_ver=$(grep "Version:" slurm/slurm.spec | head -n 1 | awk '{print $2}' | cut -d. -f-2 )
 
-echo ${distro} | xargs -n1 -I{} -P 3 ${PODMAN} build -t slurm:{} -t slurm:${slurm_ver}-{} --squash -f {}/Containerfile .
+echo ${distro} | xargs -n1 -I{} -P 3 ${PODMAN} build --pull=newer -t slurm:{} -t slurm:${slurm_ver}-{} --squash -f {}/Containerfile .
