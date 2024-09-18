853f95482a83b6ecaca9f949ce677daf444a2c36
Patrick Pun <patrickwypun04@gmail.com>
2024-09-19 00:23:20 +0100

    build deb12 image with nvml support

--- build-local.sh
+++ build-local.sh
@@ -6,3 +6,5 @@ distro=${@:-"el8 el9 deb12"}
 slurm_ver=$(grep "Version:" slurm/slurm.spec | head -n 1 | awk '{print $2}' | cut -d. -f-2 )
 
 echo ${distro} | xargs -n1 -I{} -P 3 ${PODMAN} build --pull=newer -t slurm:{} -t slurm:${slurm_ver}-{} --squash -f {}/Containerfile .
+
+${PODMAN} image prune -f
